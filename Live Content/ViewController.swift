8e298fb9a39b4bd19a75d07dadd0cfa0bb8cac8b
Snigdhaman Chatterjee <snigdhaman1991@gmail.com>
2015-12-27 23:00:01 +0530

    Initial Commit

---
+++ Live Content/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Live Content
+//
+//  Created by Chatterjee, Snigdhaman on 27/12/15.
+//  Copyright © 2015 Chatterjee, Snigdhaman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
