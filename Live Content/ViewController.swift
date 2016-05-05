7480aaa36e2e08ca3d15439685d4cbe4e757a4ca
Snigdhaman Chatterjee <snigdhaman1991@gmail.com>
2016-05-05 20:31:53 +0530

    Live Content through NSURL

--- Live Content/ViewController.swift
+++ Live Content/ViewController.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string: "http://www.stackoverflow.com")!
+        
+        webView.loadRequest(NSURLRequest(URL: url))
+        
+/*        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+            
+            if let urlcontent = data {
+                
+                let webcontent = NSString(data: urlcontent, encoding: NSUTF8StringEncoding)
+                
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    
+                    self.webView.loadHTMLString(String(webcontent!), baseURL: nil)
+                    
+                })
+                
+            }
+        }
+        
+        task.resume() */
     }
 
     override func didReceiveMemoryWarning() {
